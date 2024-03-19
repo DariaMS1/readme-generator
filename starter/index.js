9b44d3e390a7823e88c2c2d5b167fbc25263a945
daria <darias2002@gmail.com>
2024-03-19 15:56:48 +0000

    questions filled

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What's the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description of your project --->",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?",
+        default: "(npm i)"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+        default: "(npm test)"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know to use this repo?",
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What does the user need to know to contribute to this repo?",
+    },
 ];
 
 // function to write README file
